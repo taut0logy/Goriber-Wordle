acce208ba6cf18fdb50f4d472e3905177eae51cc
taut0logy <raufun.ahsan@gmail.com>
2024-02-03 09:06:41 +0600

    version 1.0

--- script/index.js
+++ script/index.js
@@ -33,15 +33,21 @@ class Trie {
     return node.isEnd;
   }
 
-  startsWith(prefix) {
+  getRandomWord() {
     let node = this.root;
-    for (let char of prefix) {
-      if (!node.children[char]) {
-        return false;
+    let word = '';
+    while (true) {
+      let childrenKeys = Object.keys(node.children);
+      if (childrenKeys.length === 0) {
+        return null; // the Trie is empty
       }
-      node = node.children[char];
+      let randomKey = childrenKeys[Math.floor(Math.random() * childrenKeys.length)];
+      word += randomKey;
+      node = node.children[randomKey];
+      if (node.isEnd) {
+        return word;
+      }
     }
-    return true;
   }
 }
 
@@ -58,18 +64,19 @@ Promise.all([
     })
     .then((json) => {
       trie.root = json;
+      word=trie.getRandomWord();
     }),
 
-  fetch("asset/words.json")
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("HTTP error " + response.status);
-      }
-      return response.json();
-    })
-    .then((json) => {
-      word = json[Math.floor(Math.random() * json.length)];
-    }),
+  // fetch("asset/words.json")
+  //   .then((response) => {
+  //     if (!response.ok) {
+  //       throw new Error("HTTP error " + response.status);
+  //     }
+  //     return response.json();
+  //   })
+  //   .then((json) => {
+  //     word = json[Math.floor(Math.random() * json.length)];
+  //   }),
 ])
   .then(() => {
     setup();
@@ -83,7 +90,12 @@ let w = 5,
 let r = 0,
   c = 0;
 
+const colors = { c: "🟩", p: "🟨", w: "⬜" };
+let guessColors = [];
+
 let over = false;
+
+let animating = false;
 
 let curSelected = null;
 
@@ -132,6 +144,7 @@ function setup() {
       else key.id = "Key" + keys[i][j];
       key.classList.add("active");
       key.addEventListener("click", (event) => {
+        if (animating) return;
         let e = { code: event.target.id };
         //console.log(e);
         processKey(e);
@@ -139,6 +152,7 @@ function setup() {
       row.appendChild(key);
     }
     document.getElementById("keyboard").appendChild(row);
+    document.getElementById("reveal").style.display = "block";
   }
 
   document.getElementById("0-0").classList.add("selected");
@@ -150,6 +164,17 @@ function setup() {
   }
 
   document.addEventListener("keyup", function (e) {
+    //console.log(e);
+    if (animating) return;
+    let id = e.code.toString();
+    if (id == "Delete" || id == "ArrowLeft") id = "Backspace";
+    const corKey = document.getElementById(id);
+    if (corKey != null) {
+      corKey.classList.add("pressed");
+      setTimeout(function () {
+        corKey.classList.remove("pressed");
+      }, 200);
+    }
     processKey(e);
   });
 }
@@ -157,13 +182,13 @@ function setup() {
 function processKey(e) {
   if (over) return;
   if ("KeyA" <= e.code && e.code <= "KeyZ") {
-    const corKey=document.getElementById("Key"+e.code[3].toString().toUpperCase());
-    corKey.classList.add("pressed");
-    setTimeout(function(){
-      corKey.classList.remove("pressed");
-    },200);
     if (c < w) {
       curSelected.innerText = e.code[3].toString().toUpperCase();
+      curSelected.classList.add("cellpressed");
+      let x=curSelected;
+      setTimeout(() => {
+        x.classList.remove("cellpressed");
+      }, 100);
       if (c < w - 1) {
         curSelected.classList.remove("selected");
       }
@@ -187,6 +212,11 @@ function processKey(e) {
       curSelected = document.getElementById(r + "-" + c);
       curSelected.classList.add("selected");
       curSelected.innerText = "";
+      curSelected.classList.add("cellpressed");
+      let x=curSelected;
+      setTimeout(() => {
+        x.classList.remove("cellpressed");
+      }, 100);
     }
   } else if (e.code == "Enter") {
     if (c != w) return;
@@ -196,8 +226,6 @@ function processKey(e) {
       guess += curSelected.innerText;
     }
     guess = guess.toLowerCase();
-    console.log(guess);
-    console.log(trie.search(guess));
     if (!trie.search(guess)) {
       let curRow = document.getElementsByClassName("currentRow");
       curRow[4].classList.remove("selected");
@@ -223,16 +251,17 @@ function processKey(e) {
         curSelected.classList.add("selected");
       }
     });
-
   }
 
   if (!over && r == h) {
     over = true;
     setTimeout(() => {
       document.getElementById("reveal").style.display = "none";
-      document.getElementById("answer").innerText =
-        "Answer: " + word + "<br/>Better luck next time.";
+      document.getElementById("answer").innerText = "Answer: " + word;
+      document.getElementById("answerText").innerText =
+        "Better luck next time!";
       document.getElementById("answer").style.display = "block";
+      document.getElementById("answerText").style.display = "block";
     }, (w - 1) * 300 + 250 + 100);
   }
 }
@@ -267,6 +296,13 @@ function update(callback) {
       state[i] = "w";
     }
   }
+
+  let guessList = {};
+
+  state.forEach((e, i) => {
+    guessList[i] = colors[e];
+  });
+  guessColors.push(guessList);
 
   setSyncAnim(state, correct);
   callback();
@@ -320,6 +356,10 @@ function setSyncAnim(state, correct) {
     cells.push(document.getElementById(r + "-" + i));
     keys.push(document.getElementById("Key" + cells[i].innerText));
   }
+  animating = true;
+  setTimeout(() => {
+    animating = false;
+  }, (w - 1) * 300 + 250 + 100);
   for (let i = 0; i < w; i++) {
     setTimeout(() => {
       flip(cells[i], keys[i]);
@@ -334,7 +374,84 @@ function setSyncAnim(state, correct) {
       document.getElementById("keyboard").style.display = "none";
       document.getElementById("reveal").style.display = "none";
       document.getElementById("answer").innerText = "You won! Congratulations!";
+      document.getElementById("answerText").innerText = getMessage(
+        calculateScore()
+      );
       document.getElementById("answer").style.display = "block";
+      document.getElementById("answerText").style.display = "block";
     }, (w - 1) * 300 + 250 + 100);
   }
+}
+
+function calculateScore() {
+  let mp = {};
+  for (let i = 0; i < w; i++) {
+    let x = word[i];
+    mp[x] = mp[x] ? mp[x] + 1 : 1;
+  }
+  let score = 0;
+  let guesses = [];
+  for (let i = 0; i < r; i++) {
+    let guess = "";
+    for (let j = 0; j < w; j++) {
+      let cell = document.getElementById(i + "-" + j);
+      guess += cell.innerText;
+    }
+    guesses.push(guess);
+  }
+  console.log(guesses);
+  let tracking = ["u", "u", "u", "u", "u"];
+  let oldGuesses = [];
+  for (let pos = 0; pos < w; pos++) {
+    for (let tries = 0; tries < r; tries++) {
+      // If the guessed position is correct
+      if (guesses[tries][pos] == word[pos]) {
+        // If the guess was not made before
+        if (tracking[pos] != "c") {
+          tracking[pos] = "c";
+          score += 50;
+        }
+        // if the guessed position was incorrect
+      } else {
+        // If the guessed position was correct before, deduct 5 points
+        if (tracking[pos] == "c") {
+          score -= 10;
+        } else if (word.includes(guesses[tries][pos])) {
+          tracking[pos] = "p";
+          //if the letter is a new letter, add 5 points
+          if (
+            oldGuesses.filter((e) => e == guesses[tries][pos]) <
+            mp[guesses[tries][pos]]
+          ) {
+            score += 10;
+            oldGuesses.push(guesses[tries][pos]);
+          }
+        }
+        //if the guess gives no new clues, no points
+        else tracking[pos] = "a";
+      }
+    }
+  }
+  //Bonus points for guessing early
+  if (tracking.filter((e) => e == "c").length == w) score += (h - r) * 50;
+  return score;
+}
+
+function getMessage(score) {
+  let message =
+    "Score:" + score + "\nTries: " + guessColors.length + "\nMoves: \n";
+  for (let i = 0; i < guessColors.length; i++) {
+    for (let j = 0; j < w; j++) {
+      message += guessColors[i][j] + " ";
+    }
+    message += "\n";
+  }
+  if (score >= 500) message += "You are a wordle legend!";
+  else if (score >= 450) message += "You are a wordle master!";
+  else if (score >= 400) message += "You are a wordle expert!";
+  else if (score >= 350) message += "You are a wordle pro!";
+  else if (score >= 300) message += "You are a wordle enthusiast!";
+  else if (score >= 250) message += "You are a wordle beginner!";
+  else message += "You are a wordle noob!";
+  return message;
 }
